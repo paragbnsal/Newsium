3dc5d7d6fee8f86c252f891a52bec0804388d84c
Parag <bansalparag114@gmail.com>
2021-09-15 23:36:14 +0530

    Added Pagination to Newsium using pageSize and totalResults

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -2,149 +2,14 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class NewsItems extends Component {
-    articles = [
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Taylor Hatmaker",
-          title:
-            "Clubhouse is adding spatial audio effects to make users feel like they're really in the room",
-          description:
-            "It’s been a busy summer for Clubhouse. The hit social audio app rolled out new messaging features and an Android app over the last few months and now the company is turning its attention to enhancing its core audio experience. Clubhouse announced Sunday that …",
-          url: "https://techcrunch.com/2021/08/29/clubhouse-spatial-audio/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/04/GettyImages-1231467577.jpg?w=600",
-          publishedAt: "2021-08-29T16:20:15Z",
-          content:
-            "Its been a busy summer for Clubhouse. The hit social audio app rolled out new messaging features and an Android app over the last few months and now the company is turning its attention to enhancing … [+4275 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Rita Liao",
-          title:
-            "China Roundup: Beijing takes aim at algorithm, Xiaomi automates electric cars",
-          description:
-            "Hello and welcome back to TechCrunch’s China roundup, a digest of recent events shaping the Chinese tech landscape and what they mean to people in the rest of the world. The biggest news of the week again comes from Beijing’s ongoing effort to dampen the infl…",
-          url: "https://techcrunch.com/2021/08/29/china-roundup-xiaomi-bytedance/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2019/10/GettyImages-695108172.jpg?w=600",
-          publishedAt: "2021-08-29T16:12:49Z",
-          content:
-            "Hello and welcome back to TechCrunchs China roundup, a digest of recent events shaping the Chinese tech landscape and what they mean to people in the rest of the world.\r\nThe biggest news of the week … [+6590 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Daniel Liss",
-          title:
-            "Move fast and break Facebook: A bull case for antitrust enforcement",
-          description:
-            "For so long, Mark Zuckerberg has told us all to move fast and break things. It’s time for him to break Facebook.",
-          url: "https://techcrunch.com/2021/08/29/move-fast-and-break-facebook-a-bull-case-for-antitrust-enforcement/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/08/GettyImages-512015968.jpeg?w=600",
-          publishedAt: "2021-08-29T14:07:26Z",
-          content:
-            "This is the second post in a series on the Facebook monopoly. The first post explored how the U.S. Federal Trade Commission should define the Facebook monopoly. I am inspired by Cloudflares recent po… [+12575 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Yifat Aran",
-          title:
-            'A new coalition for "Open Cap Table" presents an opportunity for equity transparency',
-          description:
-            "Yifat Aran Contributor Share on Twitter Dr. Yifat Aran is a visiting scholar at the Technion, Israel Institute of Technology, and an Assistant Professor in Haifa University Faculty of Law. She earned her JSD from Stanford Law School where her dissertation foc…",
-          url: "https://techcrunch.com/2021/08/29/a-new-coalition-for-open-cap-table-presents-an-opportunity-for-equity-transparency/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/08/GettyImages-111035050.jpg?w=569",
-          publishedAt: "2021-08-29T12:33:19Z",
-          content:
-            "The ownership of startups is often a mystery. In the absence of a public registry, it is difficult to figure out who owns what. Since most startups incorporate in Delaware, the Delaware Division of C… [+5905 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Jennifer Fan",
-          title: "Diversifying startups and VC power corridors",
-          description:
-            "Jennifer Fan Contributor Jennifer Fan is an assistant professor of law and director of the Entrepreneurial Law Clinic at the University of Washington School of Law. Startups have a seemingly intractable problem: a lack of diversity. Despite research showing t…",
-          url: "https://techcrunch.com/2021/08/29/diversifying-startups-and-vc-power-corridors/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/08/GettyImages-481489779.jpg?w=606",
-          publishedAt: "2021-08-29T12:32:29Z",
-          content:
-            "Startups have a seemingly intractable problem: a lack of diversity. Despite research showing that diverse founding teams have a higher rate of return than white founding teams, one characteristic of … [+6708 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Steve Gillmor",
-          title: "Gillmor Gang: Half a Loaf",
-          description:
-            "When Salesforce announced its streaming platform Salesforce+, the CRM Playaz’ Paul Greenberg and Brent Leary interviewed Colin Fleming, SVP of Global Brand Experiences at the CRM company (disclosure: I work at Salesforce). Later, I asked Brent about his show …",
-          url: "https://techcrunch.com/2021/08/28/gillmor-gang-half-a-loaf/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2020/07/F06D2685-6169-42C0-AE3B-D2545A1B3534.jpeg?w=711",
-          publishedAt: "2021-08-28T19:15:30Z",
-          content:
-            "When Salesforce announced its streaming platform Salesforce+, the CRM Playaz’ Paul Greenberg and Brent Leary interviewed Colin Fleming, SVP of Global Brand Experiences at the CRM company (disclosure:… [+5657 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Lucas Matney",
-          title:
-            "CryptoPunks blast past $1 billion in lifetime sales as NFT market goes wild",
-          description:
-            "Hello friends, and welcome back to Week in Review! Last week we dove into Bezos’s Blue Origin suing NASA. This week, I’m writing about the unlikely and triumphant resurgence of the NFT market. If you’re reading this on the TechCrunch site, you can get this in…",
-          url: "https://techcrunch.com/2021/08/28/cryptopunks-blast-past-1-billion-in-lifetime-sales-as-nft-market-goes-wild/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/04/Untitled-8.jpg?w=640",
-          publishedAt: "2021-08-28T18:48:47Z",
-          content:
-            "Hello friends, and welcome back to Week in Review! Last week we dove into Bezos’s Blue Origin suing NASA. This week, I’m writing about the unlikely and triumphant resurgence of the NFT market.\r\nIf yo… [+6983 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Natasha Mascarenhas",
-          title: "Want to be a more holistic healthcare company? Add some Ginger",
-          description:
-            "When Headspace merged with on-demand mental healthcare platform Ginger, I was surprised. After all, Ginger raised $100 million in Series E funding just a few months ago — and last time I spoke to CEO Russell Glass, he stressed the importance of integrating in…",
-          url: "https://techcrunch.com/2021/08/28/want-to-be-a-more-holistic-healthcare-company-add-some-ginger/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/06/GettyImages-1316393446.jpg?w=600",
-          publishedAt: "2021-08-28T18:00:19Z",
-          content:
-            "When Headspace merged with on-demand mental healthcare platform Ginger, I was surprised. After all, Ginger raised $100 million in Series E funding just a few months ago and last time I spoke to CEO R… [+4909 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Sarah Perez",
-          title:
-            "This Week in Apps: Developers sound off on App Store settlement, OnlyFans' flip-flop, Snap's new camera",
-          description:
-            "Welcome back to This Week in Apps, the weekly TechCrunch series that recaps the latest in mobile OS news, mobile applications and the overall app economy. The app industry continues to grow, with a record 218 billion downloads and $143 billion in global consu…",
-          url: "https://techcrunch.com/2021/08/28/this-week-in-apps-developers-sound-off-on-app-store-settlement-onlyfans-flip-flop-snaps-new-camera/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2021/08/this-week-in-apps-splash-3.png?w=753",
-          publishedAt: "2021-08-28T17:30:04Z",
-          content:
-            "Welcome back to This Week in Apps, the weekly TechCrunch series that recaps the latest in mobile OS news, mobile applications and the overall app economy.\r\nThe app industry continues to grow, with a … [+32376 chars]",
-        },
-        {
-          source: { id: "techcrunch", name: "TechCrunch" },
-          author: "Alex Wilhelm",
-          title: "The remote work argument has already been won by startups",
-          description:
-            "Welcome back to The TechCrunch Exchange, a weekly startups-and-markets newsletter for your weekend enjoyment.",
-          url: "https://techcrunch.com/2021/08/28/the-remote-work-argument-has-already-been-won-by-startups/",
-          urlToImage:
-            "https://techcrunch.com/wp-content/uploads/2020/06/NSussman_Techcrunch_Exchange-multicolor.jpg?w=533",
-          publishedAt: "2021-08-28T17:00:15Z",
-          content:
-            "Welcome back to The TechCrunch Exchange, a weekly startups-and-markets newsletter. Its inspired by what the weekday Exchange column digs into, but free, and made for your weekend reading. Want it in … [+4276 chars]",
-        }
-      ]
+    articles = []
 
       constructor(){
           super();
           this.state = {
-              articles: this.articles
+              articles: this.articles,
+              page: 1,
+              pageSize:15
           }
       }
       
@@ -154,12 +19,31 @@ export class NewsItems extends Component {
         return parsedData;        
       }
       async componentDidMount() {
-        let parsedData = await this.getPage ("https://newsapi.org/v2/everything?q=tesla&from=2021-07-31&sortBy=publishedAt&apiKey=27c49801e260406ca6f95f9cc2fc6a44");
-        this.setState({articles: parsedData.articles})  
-        console.log(parsedData);
-        
+        let parsedData = await this.getPage (`https://newsapi.org/v2/everything?q=tesla&from=2021-08-15&sortBy=publishedAt&apiKey=27c49801e260406ca6f95f9cc2fc6a44&page=${this.state.page}&pageSize=15`);
+        this.setState({articles: parsedData.articles})          
+        this.setState({totalResults: parsedData.totalResults})          
       }
       
+      handleNextClick = async () => {
+          console.log("Next")
+
+          if(this.state.page + 1 < Math.ceil(this.state.totalResults/this.state.pageSize)){
+
+            let parsedData = await this.getPage (`https://newsapi.org/v2/everything?q=tesla&from=2021-08-15&sortBy=publishedAt&apiKey=27c49801e260406ca6f95f9cc2fc6a44&page=${this.state.page + 1}&pageSize=15`);
+            this.setState({articles: parsedData.articles})  
+            this.setState({page: this.state.page + 1})  
+          }
+        
+        }
+        
+        handlePrevClick = async () => {
+        console.log("Prev")
+
+        let parsedData = await this.getPage (`https://newsapi.org/v2/everything?q=tesla&from=2021-08-15&sortBy=publishedAt&apiKey=27c49801e260406ca6f95f9cc2fc6a44&page=${this.state.page - 1}&pageSize=15`);
+          this.setState({articles: parsedData.articles})  
+          this.setState({page: this.state.page - 1})
+      }
+
   render() {
     return (
       <div className="container my-3">
@@ -170,6 +54,10 @@ export class NewsItems extends Component {
             <NewsItem title={element.title} desc = {element.description} urlToImage = {element.urlToImage} url = {element.url} author = {element.author} pubAt = {element.publishedAt}/>
           </div>
         })}
+        </div>
+        <div className="container text-center my-3">
+          <button disabled= {this.state.page<2} type="button" class="btn btn-primary mx-3" onClick={this.handlePrevClick}>Previous</button>
+          <button type="button" class="btn btn-primary mx-3" onClick={this.handleNextClick}>Next</button>
         </div>
       </div>
     );
